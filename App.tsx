171cbafc2e2cea8c70025e51dd1af32910fd971f
Md. Yousuf <57608937+Yousufjoy@users.noreply.github.com>
2023-10-14 10:10:38 +0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import CounterPage from './src/pages/CounterPage';
+
+export default function App() {
+  return (
+    <CounterPage></CounterPage>
+  );
+}
+
