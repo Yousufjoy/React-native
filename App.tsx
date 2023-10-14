e2d591d2665d7b89f4daa9a273bf6488929f7eea
Md. Yousuf <57608937+Yousufjoy@users.noreply.github.com>
2023-10-14 10:48:32 +0600

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CounterPage from './src/pages/CounterPage';
+import LoginPage from "./src/pages/LoginPage"
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <CounterPage></CounterPage>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Counter">
+                <Stack.Screen name="Counter" component={CounterPage} />
+                <Stack.Screen name="Login" component={LoginPage} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
-
--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -1,43 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import React,{useState}from "react"
+import React, { useState } from "react";
 
-export default function CounterPage() {
+export default function CounterPage(props) {
 
-    const [counter,setCounter]= useState(0)
+    const [counter, setCounter] = useState(0)
 
-    const increaseHandler = ()=>{
-        setCounter(counter+1)
+    const increaseHandler = () => {
+        setCounter(counter + 1)
         console.log(`Counter value is ${counter}`)
     }
-    
-    const decreaseHandler = ()=>{
-        setCounter(counter-1)
-    
+
+    const decreaseHandler = () => {
+        setCounter(counter - 1)
     }
 
-    const restartHandler = ()=>{
+    const restartHandler = () => {
         setCounter(0)
     }
- return (
-    <View style={styles.container}>
-      <Text>Value is : {counter}</Text>
-      <View>
-     
-        <Button title="increase" onPress={increaseHandler}></Button>
-        <Button title="decrease" onPress={decreaseHandler}></Button>
-        <Button title="Restart" onPress={restartHandler}></Button>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const navHandler = () => {
+        props.navigation.navigate("Login")
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Value is : {counter}</Text>
+            <View>
+
+                <Button title="increase" onPress={increaseHandler}></Button>
+                <Button title="decrease" onPress={decreaseHandler}></Button>
+                <Button title="Restart" onPress={restartHandler}></Button>
+            </View>
+            <View>
+                <Button title="Login" onPress={navHandler}></Button>
+            </View>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
